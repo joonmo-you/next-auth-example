1197ef75967dd5c0eceb96805530379cca22d9f2
Joonmo You <joonmo.you@gmail.com>
2022-03-22 07:33:47 +0900

    Complete next-auth option

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,5 +1,8 @@
 import NextAuth from "next-auth/next";
 import CredentialsProvider from "next-auth/providers/credentials";
+import jwt from "jsonwebtoken";
+
+const { log } = console;
 
 export default NextAuth({
   providers: [
@@ -20,4 +23,28 @@ export default NextAuth({
       },
     }),
   ],
+  secret: process.env.NEXTAUTH_SECRET,
+  session: { strategy: "jwt" },
+  jwt: {
+    secret: process.env.NEXTAUTH_SECRET,
+    maxAge: 60 * 60 * 24 * 30,
+    async encode({ token: { sub, name, email, ...token }, secret }) {
+      const signOptions = { algorithm: "HS256", expiresIn: "30 days" };
+      return jwt.sign({ sub, name, email }, secret, signOptions);
+    },
+    async decode({ token, secret }) {
+      return jwt.verify(token, secret, { algorithms: ["HS256"] });
+    },
+  },
+  callbacks: {
+    async session({ session, token, user }) {
+      return session;
+    },
+    async jwt({ token, user }) {
+      if (!token || !user) {
+        return token;
+      }
+      return { ...token, ...user };
+    },
+  },
 });
